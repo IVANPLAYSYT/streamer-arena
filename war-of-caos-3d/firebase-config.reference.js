b62cb6d89dd5e3e538a27e437ac51696dee1748a
IVANPLAYSYT <85840592+IVANPLAYSYT@users.noreply.github.com>
2026-09-12 17:50:31 +0200

    Delete war-of-caos-3d directory

--- war-of-caos-3d/firebase-config.reference.js
+++
@@ -1,9 +0,0 @@
-window.STREAMER_ARENA_FIREBASE_CONFIG = {
-  apiKey: "TU_API_KEY",
-  authDomain: "streamer-arena.firebaseapp.com",
-  databaseURL: "https://streamer-arena-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "streamer-arena",
-  storageBucket: "TU_STORAGE_BUCKET",
-  messagingSenderId: "TU_MESSAGING_SENDER_ID",
-  appId: "TU_APP_ID"
-};
